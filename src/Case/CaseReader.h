e5ffe9f682e6150d664659c28cbc94febf4dd5df
Mohamed Mousa <mohamadelbehery@icloud.com>
2026-05-21 18:04:47 +0300

    Remove noexcept from a throwing function

--- src/Case/CaseReader.h
+++ src/Case/CaseReader.h
@@ -99,7 +99,7 @@ public:
      * @return Section object
      * @note Terminates the program if section not found
      */
-    const CaseReader& section(const std::string& name) const noexcept;
+    const CaseReader& section(const std::string& name) const;
 
     /**
      * @brief Check if section exists
